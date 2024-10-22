1bdbafb7b0ee858712f1102ae868866e07c9a4df
chkoidze2 <irakli100200@gmail.com>
2024-10-22 17:33:50 +0400

    first commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,16 @@
+name = "irakli"
+surname = "chkoidze"
+
+#print(name)
+#The print function is passed an object to be displayed on the screen
+
+
+name = "irakli"
+surname = "chkoidze"
+age = 18
+height = 70
+
+loves_freedom = True 
+likes_geography = False 
+
+print(name + " " + surname + " " + str(age) + " " + str(height))
