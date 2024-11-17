40244fdbb1070037d530781578663ad3e939fd7c
chkoidze2 <irakli100200@gmail.com>
2024-11-18 03:35:40 +0400

    second commit

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,44 @@
+plus1 = 21+5
+plus2 = 12+4
+plus3 = 43+7
+plus4 = 11+9
+plus5 = 22+1
+print(plus1 , plus2 , plus3 , plus4 , plus5)
+deduc1 = 5-2
+deduc2 = 122-22
+deduc3 = 98-2
+deduc4 = 11-9
+deduc5 = 12-8
+print(deduc1 , deduc2 , deduc3 , deduc4 , deduc5)
+multip1 = 5*5
+multip2 = 9*4
+multip3 = 7*7
+multip4 = 12*3
+multipl5 = 12*12
+print(multip1 , multip2 , multip3 , multip4 , multipl5)
+d1 =  12/3
+d2 = 36/3
+d3 =  12/12
+d4 =  21/3
+print(d1 , d2 , d3 , d4 ,d4)
+
+me = 18
+mother = 40
+print(mother+20)
+print(me+20)
+
+
+
+
+name = "Zuka"     
+user_name = "Ana" 
+forage = 10                   
+mathclass = "Math"             
+print("userage") 
+age = 25
+age = age + "years"          
+Name = "Zuka"
+print(name)                  
+full_name = "Zuka K"         
+defin = "Function"           
+user_Age = 20
---
+++ level 06/classwork/classwork1.py
@@ -0,0 +1,12 @@
+name = "Zuka"     
+user_name = "Ana" 
+forage = 10                   
+mathclass = "Math"             
+print("userage") 
+age = 25
+age = age + "years"          
+Name = "Zuka"
+print(name)                  
+full_name = "Zuka K"         
+defin = "Function"           
+user_Age = 20
