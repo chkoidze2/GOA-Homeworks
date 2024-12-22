331c700fbdaaa755b1e8228ee6f3104b7d5d225d
chkoidze2 <irakli100200@gmail.com>
2024-12-22 21:22:28 +0400

    Delete level 10 directory

--- level 10/homework/homework.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter Your Name: ")
-last_name = input("Enter Your Last Name: ")
-age = (input("Enter Your Age: "))
-
-print("გამარჯობა" + " " + name + " " + last_name + "! " + "შენ ხარ" + " " + age + " " + "წლის.")
