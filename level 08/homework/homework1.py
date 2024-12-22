06af40172f6422c43c48a33dfbfd5d8aef76aa49
chkoidze2 <irakli100200@gmail.com>
2024-12-22 21:21:40 +0400

    Add files via upload

---
+++ level 08/homework/homework1.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name: ")
+last_name = input("Enter Your Last Name: ")
+age = input("Enter Your Age: ")
+
+print("Hello! My name is" + " " + name + " " + last_name + "!" + " " + "I'm" + " " + age + " " + "years old :)")
