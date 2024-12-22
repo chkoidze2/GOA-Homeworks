94dcc7af8c13494427ca9adca7ef93f27b872a39
chkoidze2 <irakli100200@gmail.com>
2024-12-22 21:22:12 +0400

    Delete level 08 directory

--- level 08/classwork/classwork.py
+++
@@ -1,9 +0,0 @@
-#input funcciit chven kopmiuters vagzavnit informacias
-name = input("Enter Your Name: ")
-print(name)
-
-word1= input("Enter Random Word: ")
-word2= input("Enter Another Random Word: ")
-
-print(word1 + " " + word2)
-#output aris ase vtqvat shedegi (rasac vxedavt ekranze)
--- level 08/homework/homework1.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter Your Name: ")
-last_name = input("Enter Your Last Name: ")
-age = input("Enter Your Age: ")
-
-print("Hello! My name is" + " " + name + " " + last_name + "!" + " " + "I'm" + " " + age + " " + "years old :)")
