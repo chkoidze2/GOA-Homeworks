ee64995e2273143dc5f93d64e9eb3ff5fba6ce6d
chkoidze2 <irakli100200@gmail.com>
2024-12-22 21:23:42 +0400

    second commit

---
+++ level 08/classwork/classwork.py
@@ -0,0 +1,9 @@
+#input funcciit chven kopmiuters vagzavnit informacias
+name = input("Enter Your Name: ")
+print(name)
+
+word1= input("Enter Random Word: ")
+word2= input("Enter Another Random Word: ")
+
+print(word1 + " " + word2)
+#output aris ase vtqvat shedegi (rasac vxedavt ekranze)
