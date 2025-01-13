b001a58e5ca44d685967ea7a2c6e0275fda3dc57
chkoidze2 <irakli100200@gmail.com>
2025-01-13 23:12:13 +0400

    second commit

---
+++ level 12/classwork/classwork.py
@@ -0,0 +1,67 @@
+num1 = 15
+num2 = 20
+
+print(num1 > num2) #False
+print(num1 < num2) #True
+print(num1 >= num2) #False
+print(num1 <= num2) #True
+print(num1 == num2) #False
+print(num1 != num2) #True
+
+num3 = 50
+num4 = 50
+
+print(num3 > num4) #False
+print(num3 < num4) #False
+print(num3 >= num4) #True
+print(num3 <= num4) #True
+print(num3 == num4) #True
+print(num3 != num4) #Flase
+
+num5 = 40
+num6 = 60
+
+print(num5 > num6) #False
+print(num5 < num6) #True
+print(num5 >= num6) #False
+print(num5 <= num6) #True
+print(num5 == num6) #False
+print(num5 != num6) #True
+
+num7 = 12
+num8 = 15
+
+print(num7 > num8) #False
+print(num7 < num8) #True
+print(num7 >= num8) #False
+print(num7 <= num8) #True
+print(num7 == num8) #False
+print(num7 != num8) #True
+
+num9 = 20
+num10 = 21
+
+print(num9 > num10) #False
+print(num9 < num10) #True
+print(num9 >= num10) #False
+print(num9 <= num10) #True
+print(num9 == num10) #False
+print(num9 != num10) #True
+
+first = 5 > 3
+print(first)
+
+second = 10 < 3
+print(second)
+
+third = 7 >= 7
+print(third)
+
+fourth = 6 <= 6
+print(fourth)
+
+fifth = 4 == 5
+print(fifth)
+
+sixth = 1 != 4
+print(sixth)
